c5642c62705f6b8580635e8a1d9450abc0bf4806
Elisaveta Zobeva <e.zobeva@gmail.com>
2022-04-14 15:03:04 +0100

    codeReview improvements

--- src/components/AttendanceForm.js
+++ src/components/AttendanceForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 function AttendanceForm(props) {
-  const houses = ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw", "Sort Me!"];
+  const options = ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw", "Sort Me!"];
   const [nameInput, setNameInput] = useState("");
   const [houseInput, setHouseInput] = useState("Gryffindor");
 
@@ -16,7 +16,7 @@ function AttendanceForm(props) {
 
   function getNewStudentHouse() {
     const randomIndex = randomHouseInd();
-    return houseInput === "Sort Me!" ? houses[randomIndex] : houseInput;
+    return houseInput === "Sort Me!" ? options[randomIndex] : houseInput;
   }
 
   function handleRegister(e) {
@@ -34,7 +34,7 @@ function AttendanceForm(props) {
   }
 
   function createHouseOptions() {
-    return houses.map((house, i) => (
+    return options.map((house, i) => (
       <option key={i} value={house}>
         {house}
       </option>
